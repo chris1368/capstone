76b0de7ca1003c21f42c2be18b7b31c2da7dd4cc
chris1368 <c-mueller.1@gmx.net>
2025-03-07 16:20:41 +0100

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,14 @@
+providers.tf
+
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.0"
+    }
+  }
+}
+ 
+provider "aws" {
+  region = "us-west-2"
+}
